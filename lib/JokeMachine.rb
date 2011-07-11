9c45a7d370a7676bb0483d337dde900a14a005e9
rennhak <github@thewebagency.org>
2011-07-11 15:59:03 +0900

    - Added JokeMachine controller stub

---
+++ lib/JokeMachine.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+#
+
+
+# The JokeMachine class controls the aquisition and display and rating of jokes.
+class JokeMachine
+
+  def initialize # {{{
+  end # of def initalize }}}
+
+end # of class JokeMachine
+
+
+# Direct Invocation
+if __FILE__ == $0 # {{{
+
+end # of if __FILE__ == $0 }}}
+
+
