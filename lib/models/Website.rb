5aba18acf51ed46a8e77e9fcddf67c1fdc11b72b
rennhak <github@thewebagency.org>
2011-07-26 21:05:33 +0900

    - Added a website model to coordinate the access frequency of polling in automatic mode

---
+++ lib/models/Website.rb
@@ -0,0 +1,15 @@
+# File: Website.rb
+
+# Meta Class to coordinate polling frequency and other options to access a particular website
+class Website
+  include DataMapper::Resource
+
+  property :id,               Serial
+  property :created_at,       DateTime
+  property :last_access,      DateTime
+
+  property :url,              String,     :length => 200,     :required => true
+
+end
+
+
